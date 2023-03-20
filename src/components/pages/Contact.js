a2e87e09efb5338531f97fa91f6ca6f41e717775
rhanciles <119437254+rhanciles@users.noreply.github.com>
2023-03-20 02:34:19 +0000

    Improve styling and add Readme
    Improve styling and adjust the CSS properties of all the pages except home.. Add Readme file

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import "../styles/Style.css";
+import "../styles/Contact.css";
 
 function Contact() {
   return (
     <div>
       <section className="pageContent jumbotron">
         <div className="col mb-4">
-          <div className="card Card">
-            <form class="card-body">
-              <h5 class="card-title">Contact Us</h5>
-              <input class="form-control" type="text" placeholder="Name"/>
-              <input class="form-control" type="text" placeholder="Email"/>
-              <input class="form-control" type="text" placeholder="Contact No."/>
-              <p class="card-text">We'll love to hear from you.</p>              
+          <div className="card mainCard">
+            <form className="card-body">
+              <h2 className="card-title">Contact Us</h2>
+              <input className="form-control" type="text" placeholder="Name"/>
+              <input className="form-control" type="text" placeholder="Email"/>
+              <input className="form-control" type="text" placeholder="Contact No."/>
+              <div className="submitDiv" >
+                <p className="card-text">We'll love to hear from you.</p> 
+                <button type="submit" className="btn btn-primary">Submit</button> 
+              </div>         
             </form>
-            <span class="contact-info">
+            <span className="contact-info">
               <ul>
-                <li><a href="#"><i class="bi bi-geo-alt"></i> Address</a></li>
+                <li><a href="#"><i className="bi bi-geo-alt"></i> Address</a></li>
                 <li><p>100 Bootstrap Street</p></li>
                 <li><p>Web City</p></li>
                 <li><p>Northend</p></li>
                 <li><p>AB1 0CD</p></li>
-                <li><a href="#"><i class="bi bi-telephone"></i> +44 7474 778 899</a></li>
-                <li><a href="#"><i class="bi bi-envelope"></i> info@rodersportal.com</a></li>
+                <li><a href="#"><i className="bi bi-telephone"></i> +44 7474 778 899</a></li>
+                <li><a href="#"><i className="bi bi-envelope"></i> info@rodersportal.com</a></li>
               </ul>      
             </span>
           </div>
